0c7a0a7e69368d040294e494505866d85904a004
Samuel Peters <65086865+Petsamuel@users.noreply.github.com>
2023-10-18 03:19:26 +0000

    [feat]: profile navigation

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,11 +8,11 @@ export const Profile = () => {
           {/* todo */}
           {/* search for profiles */}
 
-          <div className=" relative grid lg:grid-cols-3 md:grid-cols-2 w-54 h-16 ">
+          <div className=" relative grid lg:grid-cols-3 md:grid-cols-2 w-54 h-24 ">
             {/* card for profile */}
             {data.map((value, key) => (
               <div
-                className="border  rounded-lg my-8 px-4 py-6 items-start flex gap-4 dm-bg-rev"
+                className="border  rounded-lg my-8 px-4 py-6 items-start flex gap-4 dm-bg mx-8"
                 key={key}
               >
                 <img
@@ -23,19 +23,19 @@ export const Profile = () => {
 
                 <div className="profile-details flex flex-col mx-4 my-4 flex-auto">
                   <div className="profie name w-full flex">
-                    <h1 className=" lg:text-2xl text-xl text-bold dm-text-rev">
+                    <h1 className=" lg:text-2xl text-xl font-bold dm-text">
                       {value.name}
                     </h1>
                   </div>
 
-                  <p className="py-5 w-full flex dm-text-rev lg:text-md text-sm">
+                  <p className="py-5 w-full flex dm-text lg:text-md text-sm">
                     {value.description}
                   </p>
 
-                  <p className="profile-description py-2 lg:text-md text-sm dm-text-rev">
+                  <p className=" py-2 lg:text-md text-sm dm-text font-base uppercase">
                     {value.stack}
                   </p>
-                  <div className="profile-link py-2 dm-text-rev lg:text-md text-sm cursor pointer">
+                  <div className="profile-link py-2 dm-text lg:text-md text-sm cursor pointer">
                     Connect
                   </div>
                 </div>
