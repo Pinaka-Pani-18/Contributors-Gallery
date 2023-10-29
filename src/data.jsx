79006660562ba0570fd99e3a5db790e59c681c05
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-10-30 00:03:49 +0530

    profile link added

--- src/data.jsx
+++ src/data.jsx
@@ -2,9 +2,9 @@ export const data = [
   {
     name: "Pinaka Pani",
     skills: "REACT, Java, TailwindCSS ... ",
-    description: `Frontend developer who loves creating projects.`,
+    description: `Frontend developer, Programmer and Open Source Enthusiast`,
     socials: "https://github.com/Pinaka-Pani-18",
-    image: "https://github.com/account",
+    image: "https://avatars.githubusercontent.com/u/102857774?v=4",
   },
   {
     name: "Peter Samuel",
